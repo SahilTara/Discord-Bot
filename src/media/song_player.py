12726da20f2e847d983bc0c7c862d183c241de2f
Sahil Tara <supertfriend@gmail.com>
2018-08-11 21:21:38 -0400

    1) Fixed bug of videos losing validity after about 10 minutes of streaming. 2) Changed the current song queue from a sleep wait methodology to a lock and acquire methodology 3) Allowed for vote Skipping 4) Fixed a small bug with the stop command that made the next song attempt to play

--- src/media/song_player.py
+++ src/media/song_player.py
@@ -7,13 +7,12 @@ import youtube_dl
 
 class YoutubeSource(discord.PCMVolumeTransformer):
     def __init__(self, source, *, info, volume=0.8):
+        super().__init__(source, volume)
         self.uploader = info.get('uploader')
         self.views = info.get('views')
         self.likes = info.get('likes')
         self.title = info.get('title')
         self.duration = info.get('duration')
-        self.original = source
-        self.volume = volume
 
     @classmethod
     async def from_url(cls, url: str, *, loop: asyncio.AbstractEventLoop = None, opts: dict = None, **kwargs):
@@ -55,21 +54,30 @@ class Music:
         self.bot = bot
         self.vc: discord.VoiceClient = None
         self.current = None
-        self.play_next: asyncio.Event = asyncio.Event()  # TODO: Use this to implement lock mechanism instead of sleep!
+        self.play_next: asyncio.Event = asyncio.Event()  # Acts kind of like a lock mechanism, but can wake multiple.
         self.songs: asyncio.Queue = asyncio.Queue()  # We have to queue the songs of course
         self.audio_player = self.bot.loop.create_task(self.audio_task())
+        self.skips = set()
+
+    def toggle_lock(self, error=None):
+        if error is not None:
+            print(error)
+        self.bot.loop.call_soon_threadsafe(self.play_next.set)
 
     async def audio_task(self):
         while True:
             if self.vc:
+                self.play_next.clear()  # Set the event to false (kind of like acquiring a lock
+
                 self.current, ctx = await self.songs.get()
-                print(self.current.uploader)
+
                 if ctx is not None:
                     await ctx.send(f"Now Playing: {str(self.current)} requested by:{ctx.message.author}")
-                self.vc.play(self.current)
-                await asyncio.sleep(self.current.duration)
+
+                self.vc.play(self.current, after=self.toggle_lock)
+                await self.play_next.wait()
             else:
-                await asyncio.sleep(1) #TODO: replace this sleep mechanism with locks
+                await asyncio.sleep(0)  # Needs some type of await
 
     @commands.command(no_pm=True)
     async def play(self, ctx, link: str):
@@ -86,8 +94,11 @@ class Music:
         if channel is not None:
             if self.vc is None:
                 self.vc = await channel.connect()
-
-            player = await YoutubeSource.from_url(link, loop=self.bot.loop)
+            args = "-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5"
+            player = await YoutubeSource.from_url(url=link,
+                                                  loop=self.bot.loop,
+                                                  opts=None,
+                                                  before_options=args)
             await ctx.send("Enqueued {0} requested by:{1}".format(str(player), ctx.message.author))
             await self.songs.put((player, ctx))
 
@@ -97,7 +108,29 @@ class Music:
     @commands.command(no_pm=True)
     async def stop(self, ctx):
         if self.vc is not None:
+            await ctx.send("Clearing song queue, and leaving the voice channel")
+
+            self.songs = asyncio.Queue()
             await self.vc.disconnect()
             self.vc = None
         else:
             await ctx.send("Sorry. I am not in a channel right now!")
+
+    @commands.command(no_pm=True)
+    async def skip(self, ctx):
+        skip_size = 2
+        if self.vc and self.vc.is_playing():
+            author: discord.Member = ctx.message.author
+            if len(self.skips) > skip_size:
+                return
+            if author.id not in self.skips:
+                self.skips.add(author.id)
+                await ctx.send("Voted to skip song! ({0}/{1})".format(len(self.skips), skip_size))
+            else:
+                await ctx.send("You have already voted!")
+            if len(self.skips) == skip_size:
+                await ctx.send("Now skipping song!")
+                self.vc.stop()
+                self.skips.clear()
+        else:
+            await ctx.send("I am not playing anything right now!")
